097ee66a28551d657774b0e9b41e7ed381f367c4
loiane <loianeg@gmail.com>
2017-04-13 20:15:31 -0300

    updated tasks component with dispatched actions - container component

--- angular-ngrx-todo/src/app/tasks/components/tasks/tasks.component.ts
+++ angular-ngrx-todo/src/app/tasks/components/tasks/tasks.component.ts
@@ -1,3 +1,6 @@
+import { Task } from './../../model/task';
+import { Observable } from 'rxjs/Observable';
+import { TasksStoreService } from './../../services/tasks-store.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  tasks$: Observable<Task[]>;
+
+  constructor(private taskStoreService: TasksStoreService) { }
 
   ngOnInit() {
+    this.tasks$ = this.taskStoreService.tasks$;
+  }
+
+  onCreateTask(title) {
+    this.taskStoreService.createTask(title);
+  }
+
+  onRemoveTask(task) {
+    this.taskStoreService.removeTask(task);
+  }
+
+  onUpdateTask(event) {
+    this.taskStoreService.updateTask(event.task, event.updates);
   }
 
 }
