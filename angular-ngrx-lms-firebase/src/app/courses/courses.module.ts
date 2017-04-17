4f2f9fe5f144fcaf417ae70bd9462259041bf8c5
loiane <loianeg@gmail.com>
2017-04-17 14:45:53 -0300

    created courses module

---
+++ angular-ngrx-lms-firebase/src/app/courses/courses.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoursesRoutingModule
+  ],
+  declarations: []
+})
+export class CoursesModule { }
