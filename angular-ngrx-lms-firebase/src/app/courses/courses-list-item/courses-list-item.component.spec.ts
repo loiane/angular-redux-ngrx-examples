09af917da3f1ab9bd0c32602bcfb9d2a46cc91f3
loiane <loianeg@gmail.com>
2017-04-18 09:13:04 -0300

    added course list item component

---
+++ angular-ngrx-lms-firebase/src/app/courses/courses-list-item/courses-list-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoursesListItemComponent } from './courses-list-item.component';
+
+describe('CoursesListItemComponent', () => {
+  let component: CoursesListItemComponent;
+  let fixture: ComponentFixture<CoursesListItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CoursesListItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CoursesListItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
