1c2fbf48ee48cd9bd2c60e50096d2a1e71003d35
loiane <loianeg@gmail.com>
2017-04-20 14:56:26 -0300

    changed load lesson from firebase to ngrx

--- angular-ngrx-lms-firebase/src/app/lessons/lesson-detail/lesson-detail.component.ts
+++ angular-ngrx-lms-firebase/src/app/lessons/lesson-detail/lesson-detail.component.ts
@@ -1,7 +1,12 @@
+import { Lesson } from './../models/lesson';
+import { Observable } from 'rxjs/Rx';
 import { Subscription } from 'rxjs/Rx';
 import { LessonsService } from './../lessons.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromRoot from './../../ngrx/reducers';
+import * as actions from './../store/lessons.actions';
 
 @Component({
   selector: 'app-lesson-detail',
@@ -10,21 +15,27 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class LessonDetailComponent implements OnInit, OnDestroy {
 
-  lesson;
+  lesson$: Observable<Lesson>;
   private subscription: Subscription;
 
   constructor(
-    private router: Router,
     private route: ActivatedRoute,
-    private lessonsService: LessonsService
-  ) { }
+    private store: Store<fromRoot.AppState>
+  ) {
+    this.subscription = this.route.params
+      .select<string>('url')
+      .map(url => new actions.LessonSelectedAction({url}))
+      .subscribe(this.store);
+   }
 
   ngOnInit() {
-    this.subscription = this.route.params.switchMap(params => {
+    /*this.subscription = this.route.params.switchMap(params => {
       const lessonUrl = params['url'];
       return this.lessonsService.findLessonByUrl(lessonUrl);
     })
-      .subscribe(lesson => this.lesson = lesson);
+      .subscribe(lesson => this.lesson = lesson);*/
+
+    this.lesson$ = this.store.select(fromRoot.getSelectedLesson);
   }
 
   ngOnDestroy() {
