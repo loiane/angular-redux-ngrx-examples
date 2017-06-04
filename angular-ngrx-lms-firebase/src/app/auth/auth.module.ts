7c3bdabab85730ab1f4809571be0f5b80c036997
loiane <loianeg@gmail.com>
2017-06-04 13:19:28 -0300

    updated angularfire2

--- angular-ngrx-lms-firebase/src/app/auth/auth.module.ts
+++ angular-ngrx-lms-firebase/src/app/auth/auth.module.ts
@@ -1,19 +1,20 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthEffects } from './store/auth.effects';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AuthService } from './services/auth.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { EffectsModule } from '@ngrx/effects';
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     EffectsModule.run(AuthEffects)
   ],
@@ -22,7 +23,8 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   providers: [
-    AuthService
+    AuthService,
+    AngularFireAuth
   ]
 })
 export class AuthModule { }
