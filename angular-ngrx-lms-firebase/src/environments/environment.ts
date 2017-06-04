6c43cbf4dba48d352a659f1357b3f9d2b0656f58
loiane <loianeg@gmail.com>
2017-06-04 13:17:24 -0300

    updated angularfire2

--- angular-ngrx-lms-firebase/src/environments/environment.ts
+++ angular-ngrx-lms-firebase/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAWVytc72KwMjGGwgwfsF11UAPXh4SW248',
+    authDomain: 'angular-lms-48d70.firebaseapp.com',
+    databaseURL: 'https://angular-lms-48d70.firebaseio.com',
+    projectId: 'angular-lms-48d70',
+    storageBucket: 'angular-lms-48d70.appspot.com',
+    messagingSenderId: '728567730560'
+  }
 };
